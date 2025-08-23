63cac63c948cefdad471f7b18f75124d0d905513
henkvanvoorst92 <henkvanvoorst92@gmail.com>
2025-08-22 20:35:01 -0700

    first

---
+++ AJNR_review_time.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    file = '/home/hvv/Documents/reviews/AJNR/Reviewer Stats_v2.xlsx'
+    df = pd.read_excel(file, sheet_name=None)
+
+
+
+
+
+
+    print(1)
+
